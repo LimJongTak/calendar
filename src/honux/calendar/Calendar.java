afbdd473949d6294b4f7519b98861d022f4b1b28
Administrator <Administrator@jongtak-PC>
2018-06-06 22:29:07 +0900

    Update Calendar

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -2,13 +2,68 @@ package honux.calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("일  월  화  수 목  금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-
+		System.out.println("------------------------------------------------ ");
+		System.out.println("|      2018년 1월       | |     2018년 2월           |");
+		System.out.println("|일  월  화  수 목  금  토     일  월 화  수 목  금  토    ");
+		System.out.println("|-------------------- | |-----------------------|");
+		System.out.println("|    1  2  3  4  5  6 | |             1  2  3   |");
+		System.out.println("| 7  8  9 10 11 12 13 | | 4  5  6  7  8  9 10   |");
+		System.out.println("|14 15 16 17 18 19 20 | |11 12 13 14 15 16 17   |");
+		System.out.println("|21 22 23 24 25 26 27 | |18 19 20 21 22 23 24   |");
+		System.out.println("|28 29 30 31          | |25 26 27 28            |");
+		System.out.println("-------------------------------------------------");
+		System.out.println("------------------------------------------------ ");
+		System.out.println("|      2018년 3월        | |     2018년 4월           |");
+		System.out.println("| 일  월  화  수 목  금  토     일  월 화  수 목  금   토    ");
+		System.out.println("|--------------------  | |-----------------------|");
+		System.out.println("|              1  2  3 | | 1  2  3  4  5  6   7  |");
+		System.out.println("| 4   5  6  7  8  9 10 | | 8  9 10 11 12 13  14  |");
+		System.out.println("|11  12 13 14 15 16 17 | |15 16 17 18 19 20  21  |");
+		System.out.println("|18  19 20 21 22 23 24 | |22 23 24 25 26 27  28  |");
+		System.out.println("|25  26 27 28 29 30 31 | |29 30                  |");
+		System.out.println("--------------------------------------------------");
+		System.out.println("-------------------------------------------------- ");
+		System.out.println("|      2018년 5월        | |     2018년 6월           |");
+		System.out.println("| 일  월 화  수 목  금   토     일  월 화  수 목  금   토   ");
+		System.out.println("|--------------------  | |-----------------------|");
+		System.out.println("|       1  2  3  4   5 | |                1   2  |");
+		System.out.println("| 6  7  8  9 10 11  12 | | 3  4  5  6  7  8   9  |");
+		System.out.println("|13 14 15 16 17 18  19 | |10 11 12 13 14 15  16  |");
+		System.out.println("|20 21 22 23 24 25  26 | |17 18 19 20 21 22  23  |");
+		System.out.println("|27 28 29 30 31        | |24 25 26 27 28 29  30  |");
+		System.out.println("--------------------------------------------------");
+		System.out.println("-------------------------------------------------- ");
+		System.out.println("|      2018년 7월        | |     2018년 8월           |");
+		System.out.println("| 일 월  화  수 목  금  토     일  월  화  수  목  금  토    ");
+		System.out.println("|--------------------  | |-----------------------|");
+		System.out.println("| 1  2  3  4  5  6   7 | |           1  2  3  4  |");
+		System.out.println("| 8  9 10 11 12 13  14 | | 5  6  7   8  9 10 11  |");
+		System.out.println("|15 16 17 18 19 20  21 | |12 13 14  15 16 17 18  |");
+		System.out.println("|22 23 24 25 26 27  28 | |19 20 21  22 23 24 25  |");
+		System.out.println("|29 30 31              | |26 27 28  29 30 31     |");
+		System.out.println("--------------------------------------------------");
+		System.out.println("-------------------------------------------------- ");
+		System.out.println("|      2018년 9월        | |     2018년10월           |");
+		System.out.println("| 일  월  화  수 목  금  토     일  월 화  수 목  금  토    ");
+		System.out.println("|--------------------  | |-----------------------|");
+		System.out.println("|                    1 | |    1  2  3  4  5  6   |");
+		System.out.println("| 2   3  4  5  6  7  8 | | 7  8  9 10 11 12 13   |");
+		System.out.println("| 9  10 11 12 13 14 15 | |14 15 16 17 18 19 20   |");
+		System.out.println("|16  17 18 19 20 21 22 | |21 22 23 24 25 26 27   |");
+		System.out.println("|23  24 25 26 27 28 29 | |28 29 30 31            |");
+		System.out.println("|30                    | |                       |");
+		System.out.println("--------------------------------------------------");
+		System.out.println("-------------------------------------------------- ");
+		System.out.println("|      2018년 11월       | |     2018년 12월          |");
+		System.out.println("| 일  월  화  수 목  금  토     일  월 화  수 목  금  토    ");
+		System.out.println("|--------------------  | |-----------------------|");
+		System.out.println("|              1  2  3 | |                   1   |");
+		System.out.println("| 4   5  6  7  8  9 10 | | 2  3  4  5  6  7  8   |");
+		System.out.println("|11  12 13 14 15 16 17 | | 9 10 11 12 13 14 15   |");
+		System.out.println("|18  19 20 21 22 23 24 | |16 17 18 19 20 21 22   |");
+		System.out.println("|25  26 27 28 29 30    | |23 24 25 26 27 28 29   |");
+		System.out.println("|                      | |30 31                  |");
+		System.out.println("--------------------------------------------------");
 	}
 
 }
